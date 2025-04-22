f83a08ec419b738dc5214de96a3f968a4c60f1ff
Dmitry <X.x.xh@yandex.ru>
2025-04-22 23:50:45 +0300

    first line

--- Lab1/main/main.c
+++ Lab1/main/main.c
@@ -15,6 +15,7 @@ typedef struct State
     int j;
     int k;
     int s;
+    double u[3];
 }State;
 
 
@@ -45,18 +46,35 @@ double main_method(double** net, int j, int m){
 }
 
 
+double print_res(double* f){
+    printf("\n");
+    for(int i = 0, pos = 0; i < M; i++){
+        for(int j = 0; j < K; j++, pos++){
+            printf("%3.3lf ", f[pos]);
+        }
+        printf("\n");
+    }
+}
+
+
 int main(int argc, char** argv){
 
     if (argc < 5)
+    return EXIT_FAILURE;
+    
+    if (!(T = atof(argv[1])))
         return EXIT_FAILURE;
 
-    if (!(K = atoi(argv[1]) && M == atoi(argv[2])))
+    if (!(X = atof(argv[2])))
+        return EXIT_FAILURE;
+
+    if (!(K = atoi(argv[3])))
+        return EXIT_FAILURE;
+
+    if (!(M = atoi(argv[4])))
         return EXIT_FAILURE;
 
     if(!M || !K)
-        return EXIT_FAILURE;
-
-    if (!(K = atof(argv[3]) && M == atof(argv[4])))
         return EXIT_FAILURE;
 
     t = T/(double)M;
@@ -65,21 +83,66 @@ int main(int argc, char** argv){
     int commsize, rank;
     double proc_time = 0;
 
-
+    K++;
+    M++;
+    int total = K*M;
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    MPI_Status status;
+
+    int executors = commsize-1;
+    if (executors > M)
+        executors = M;
 
     if (!rank){
-        int  *planner    = (State*)calloc(M*K, sizeof(int*));
-        int  *result   = (int*)calloc(M*K, sizeof(int*));
+        //stage 1
+        proc_time -= MPI_Wtime();
+        double *f = (double*)calloc(total, sizeof(double));
 
-        for()
+        int cycles = M / executors;
+        int overload = M % executors;
+        int executors_num = 0;
+
+        for(int i = 0; i < total; i++)
+            f[i] = 0;
+
+        for(int i = 0, T = 0, X = 0; i < cycles + (overload>0); i++){
+            executors_num = (i==cycles)?overload:executors;
+            for(int j = 0; j < executors_num; j++, T++){
+                MPI_Send((void*)&T, 1, MPI_INTEGER, (T)%executors+1, 0, MPI_COMM_WORLD);
+                MPI_Send((void*)&X, 1, MPI_INTEGER, (T)%executors+1, 0, MPI_COMM_WORLD);
+            }
+            T -= executors_num;
+            for(int j = 0; j < executors_num; j++, T++){
+                MPI_Recv((void*)&(f[T]), 1, MPI_DOUBLE, (T)%executors+1, 0, MPI_COMM_WORLD, &status);
+                //print_res(f);
+            }
+                
+        }
+        
+        free(f);
+        proc_time += MPI_Wtime();
+        printf("execution time:%lf\n", proc_time);
+    }
+
+    else if(rank < M+2){
+        int cycles = M / executors + (rank <= M%executors);
+        int X = 0, T = 0;
+        double f_val;
+        for(int i = 0; i < cycles; i++){
+            MPI_Recv((void*)&(T), 1, MPI_INTEGER, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv((void*)&(X), 1, MPI_INTEGER, 0, 0, MPI_COMM_WORLD, &status);
+            f_val = phi(T/t);
+            MPI_Send((void*)&f_val, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
 
     }
     
 
-
+    MPI_Finalize();
 
     return 0;
 }
