c3614eccbf7224e62b97bac4a5f61f96c66dcc49
Dmitry <X.x.xh@yandex.ru>
2025-04-22 16:38:18 +0300

    fixed

--- Lab1/pass.c
+++ Lab1/pass.c
@@ -1,12 +1,14 @@
 #include <mpi.h>
 #include <stdlib.h>
-#include <stdout.h>
+#include <stdio.h>
 
 #define TEST_CONST 10000
 
 int main(int argc, char** argv){
     double total_time, mean_time, buf = 0;
     int commsize, rank;
+
+    MPI_Status status;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
@@ -16,8 +18,8 @@ int main(int argc, char** argv){
         for (int i = 0; i < TEST_CONST; i++){
             total_time = -MPI_Wtime();
 
-            MPI_Send((void*)&buf, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-            MPI_Recv((void*)&buf, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Send((void*)&buf, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv((void*)&buf, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &status);
 
             total_time += MPI_Wtime();
             mean_time += total_time;
@@ -28,8 +30,8 @@ int main(int argc, char** argv){
 
     if (rank == 1){
         for(int i = 0; i < TEST_CONST; i++){
-            MPI_Send((void*)&buf, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
             MPI_Recv((void*)&buf, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+            MPI_Send((void*)&buf, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
     }
 
